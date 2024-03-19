c085288d505b1fd586af5ce5a911c661c09a8893
Eugene Gatsak <greenglaz@gmail.com>
2024-03-20 00:12:32 +0100

    fix: update typeorm config file

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,13 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
 
 export const typeOrmConfig = {
   type: 'postgres',
-  // TODO fix: make env available to typeorm migrations generator
-  host: process.env.POSTGRES_HOST ?? 'localhost',
-  port: +process.env.POSTGRES_PORT ?? 5432,
-  username: process.env.DATABASE_USER ?? 'postgres',
-  password: process.env.DATABASE_PASSWORD ?? 'postgres',
-  database: process.env.DATABASE_NAME ?? 'rest_service',
+  host: process.env.POSTGRES_HOST_DATASOURCE,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
   migrationsRun: false,
   migrationsTableName: 'migration',
@@ -17,12 +17,3 @@ export const typeOrmConfig = {
 } satisfies DataSourceOptions;
 
 export const appDataSource = new DataSource(typeOrmConfig);
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log('Data Source has been initialized');
-  })
-  .catch((err) => {
-    console.error('Error during Data Source initialization', err);
-  });
